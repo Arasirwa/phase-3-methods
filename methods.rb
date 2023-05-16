b461b56d5900ddb11bd8617f33c36be4097fb21c
william arasirwa <mageboarasirwa03@gmail.com>
2023-05-16 12:04:32 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,44 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+
+    puts "Hello, #{name}!"
+
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum = add(1,7)
+
+
+def halve(number)
+
+    if number.class != Integer
+        return nil
+    end
+
+    number/2
+end
+
+# def halve(number)
+#   return nil unless number.is_a?(Numeric)
+
+#   number / 2
+# end
